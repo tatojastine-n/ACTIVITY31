eeba31cdd91593c4e8691d71442f92ef3261fe4b
tatojastine-n <jajanicoletato@gmail.com>
2025-08-08 22:06:26 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeWordChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(word))
+            {
+                Console.WriteLine("Error: No word entered.");
+                return;
+            }
+            bool isPalindrome = true;
+            int length = word.Length;
+
+            // Check characters from start to end
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (word[i] != word[length - 1 - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not a palindrome");
+            }
+        }
+    }
+}
